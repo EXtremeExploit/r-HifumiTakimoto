201bf880508b0f97b3474a6d5eb5377955e1e3f9
EXtremeExploit <gdextremeexploit@gmail.com>
2018-04-30 14:53:22 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+var dotenv = require('dotenv');
+dotenv.config();
+var _reddit = require('snoowrap');
+var snoostorm = require('snoostorm');
+var _twit = require('twit');
+var isgd = require('isgd');
+var r = new _reddit({
+    username: process.env.reddit_username,
+    password: process.env.reddit_password,
+    userAgent: 'r/HifumiTakimoto Twitter Bot',
+    clientId: process.env.reddit_clientID,
+    clientSecret: process.env.reddit_clientSecret
+});
+const twit = new _twit({
+    access_token: process.env.twitter_accesToken,
+    access_token_secret: process.env.twitter_accesTokenSecret,
+    consumer_key: process.env.twitter_consumerKey,
+    consumer_secret: process.env.twitter_consumerSecret,
+});
+
+const client = new snoostorm(r);
+client.SubmissionStream({
+    subreddit: 'TakimotoHifumi'
+}).on('submission', sub => {
+    isgd.shorten('https://www.reddit.com/' + sub.subreddit_name_prefixed + '/' + sub.id, (link) => {
+        twit.post('statuses/update', {
+            status: '(' + link + ') ' + sub.title + ' ' + sub.url
+        }).then((err) => {
+            console.log(err);
+        }).catch(err => console.log(err));
+    });
+
+});
